35f8d3d3c5a33371037503daab59793c7960933d
Jack Zhang <jackzhang@Jacks-MacBook-Pro-4.local>
2026-03-24 23:53:38 -0400

    init commit

---
+++ newton_schulz/__init__.py
@@ -0,0 +1,10 @@
+from .standard_newton_schulz import StandardNewtonSchulz
+from .gram_newton_schulz import GramNewtonSchulz
+from .coefficients import YOU_COEFFICIENTS, POLAR_EXPRESS_COEFFICIENTS
+
+__all__ = [
+    "StandardNewtonSchulz",
+    "GramNewtonSchulz",
+    "YOU_COEFFICIENTS",
+    "POLAR_EXPRESS_COEFFICIENTS",
+]
